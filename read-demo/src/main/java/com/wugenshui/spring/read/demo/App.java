dc4e98e5de58ffb161f698bb516538aef9f3c209
chenbo <chenbo@huaweisoft.com>
2021-09-26 10:21:30 +0800

    read-demo

---
+++ read-demo/src/main/java/com/wugenshui/spring/read/demo/App.java
@@ -0,0 +1,43 @@
+package com.wugenshui.spring.read.demo;
+
+//import org.springframework.core.io.ClassPathResource;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+public class App {
+	public static void main(String[] args) throws IOException {
+//		ClassPathResource resource = new ClassPathResource("spring.xml");
+//		System.out.println("resource = " + resource);
+//		ClassPathResource resource = new ClassPathResource("spring.xml");
+//		InputStream inputStream = resource.getInputStream();
+//
+//		System.out.println(readInputStream(inputStream));
+//
+//		System.out.println(resource.exists());
+//		System.out.println(resource.contentLength());
+//		System.out.println(resource.getFilename());
+//		System.out.println(resource.lastModified());
+//		System.out.println(resource.getDescription());
+//		System.out.println(resource.isOpen());
+//		System.out.println(resource.isReadable());
+
+//		BeanFactory bf = new XmlBeanFactory(new ClassPathResource("spring.xml"));
+//		MyTestBean bean = (MyTestBean) bf.getBean("MyTestBean");
+//		System.out.println("bean = " + bean.getTestStr());
+		System.out.println("init");
+	}
+
+	public static String readInputStream(InputStream inputStream) throws IOException {
+		byte[] buffer = new byte[1024];
+		int len = 0;
+		ByteArrayOutputStream bos = new ByteArrayOutputStream();
+		while ((len = inputStream.read(buffer)) != -1) {
+			bos.write(buffer, 0, len);
+		}
+		bos.close();
+		final byte[] bytes = bos.toByteArray();
+		return new String(bytes);
+	}
+}
---
+++ read-demo/src/main/java/com/wugenshui/spring/read/demo/MyTestBean.java
@@ -0,0 +1,14 @@
+package com.wugenshui.spring.read.demo;
+
+public class MyTestBean {
+
+    private String testStr = "testStr";
+
+    public String getTestStr() {
+        return testStr;
+    }
+
+    public void setTestStr(String testStr) {
+        this.testStr = testStr;
+    }
+}
